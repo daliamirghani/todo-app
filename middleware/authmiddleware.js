64acd2923f77ce89b9dc6e26c4a4302082feb91e
dalia <daliamirghani@gmail.com>
2025-03-10 11:50:48 +0200

    added authentication middleware

---
+++ middleware/authmiddleware.js
@@ -0,0 +1,43 @@
+const jwt = require("jsonwebtoken");
+const userData= require("../modules/users.modules.js");
+
+const doubleAuth = async (req,res,next)=>{ //both authentication and authorization
+const token = req.cookies.token;
+if (!token)
+{
+    return res.status(401).json({
+        status: 401,
+        msg: "Unauthorized access!"
+    })
+}
+let decoded= jwt.verify(token, "task9");
+if (!decoded)
+{
+     return res.status(401).json({
+        status: 401,
+        msg: "Unauthorized access!"
+})
+}
+const userId=mongoose.Types.ObjectId(decoded.userId);
+try {
+    const userObj = await userData.findById(userId).select("-password");
+    if (!userObj)
+ {
+    return res.status(404).json({
+        status: 404,
+        msg: "User not found!"})
+ }
+ req.userId = userId; //only sends it after it ensures the user exists
+ next();
+}
+catch (error) {
+    return res.status(500).json({
+        status: 500,
+        msg: "Error loading user data!"
+})
+}
+}
+
+module.exports={
+    doubleAuth
+}
